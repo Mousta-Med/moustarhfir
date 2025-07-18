7f18fca5ebc74115c0ba3e236731476991f67ee8
Mousta-Med <moustamuhammed@gmail.com>
2025-07-18 17:27:45 +0100

    feat: add navigation menu component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   return (
     <>
-      <Button variant="outline">Click Me</Button>
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Button variant="outline">Click Me</Button>
+      </div>
     </>
   );
 }
